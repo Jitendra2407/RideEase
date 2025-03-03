ccc577b0c7e9c52816b13c9823720bc76b7ccd45
Jitendra2407 <jitendrakumarthakur552@gmail.com>
2025-03-03 22:37:02 +0530

    registration captain done

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 
 connectToDb();
 
@@ -14,10 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
 
 module.exports = app;
